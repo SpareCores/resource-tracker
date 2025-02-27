4f8199ad1f18e68c520a6a1d28f1d0ad6b2e9eb1
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-02-27 13:35:06 +0100

    promote submodule

--- src/metaflow_extensions/resource_tracker/plugins/mfextinit_resource_tracker.py
+++ src/metaflow_extensions/resource_tracker/plugins/mfextinit_resource_tracker.py
@@ -1,5 +1,7 @@
 STEP_DECORATORS_DESC = [
-    ("track_resources", ".track_resources.track_resources_decorator.ResourceTrackerDecorator"),
+    (
+        "track_resources",
+        ".track_resources.track_resources_decorator.ResourceTrackerDecorator",
+    ),
 ]
-# __mf_promote_submodules__ = ["resource_tracker"]
-# __mf_dependencies__ = ["resource_tracker"]
+__mf_promote_submodules__ = ["track_resources"]
